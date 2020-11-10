8888bb496c80fcce164a536785550f3a6982c4ca
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2020-11-10 21:04:09 +0800

    update

--- ganbert.py
+++ ganbert.py
@@ -74,7 +74,7 @@ flags.DEFINE_bool(
     "do_predict", False,
     "Whether to run the model in inference mode on the test set.")
 
-flags.DEFINE_integer("train_batch_size", 8, "Total batch size for training.")
+flags.DEFINE_integer("train_batch_size", 64, "Total batch size for training.")
 
 flags.DEFINE_integer("eval_batch_size", 8, "Total batch size for eval.")
 
